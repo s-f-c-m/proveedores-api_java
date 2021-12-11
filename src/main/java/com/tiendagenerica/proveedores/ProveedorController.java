b90afd3f6fb85d077980023cd5aa386f8ae3fcb5
Sebastian Castano <sebastian9111@gmail.com>
2021-12-11 22:46:39 +0200

    add jwt not valid exception

--- src/main/java/com/tiendagenerica/proveedores/ProveedorController.java
+++ src/main/java/com/tiendagenerica/proveedores/ProveedorController.java
@@ -4,6 +4,7 @@ package com.tiendagenerica.proveedores;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.security.auth.message.AuthException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -18,39 +19,47 @@ public class ProveedorController {
     private final JWTChecker jwtchecker;
 
     @PostMapping
-    public void insertProveedor(@RequestHeader Map<String, String> headers, @RequestBody Proveedor proveedor){
+    public void insertProveedor(@RequestHeader Map<String, String> headers, @RequestBody Proveedor proveedor) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             proveedorService.insertProveedor(proveedor);
+        }else{
+            throw new AuthException("No authorizado");
         }
     }
 
     @GetMapping
-    public List<Proveedor> getProveedores(@RequestHeader Map<String, String> headers){
+    public List<Proveedor> getProveedores(@RequestHeader Map<String, String> headers) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             return proveedorService.getAll();
+    }else{
+        throw new AuthException("No authorizado");
         }
-        return null;
     }
 
     @GetMapping("/{id}")
-    public Optional<Proveedor> getProveedor(@RequestHeader Map<String, String> headers, @PathVariable String id){
+    public Optional<Proveedor> getProveedor(@RequestHeader Map<String, String> headers, @PathVariable String id) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             return proveedorService.getProveedor(id);
+        }else{
+        throw new AuthException("No authorizado");
         }
-        return null;
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProveedor(@RequestHeader Map<String, String> headers, @PathVariable String id){
+    public void deleteProveedor(@RequestHeader Map<String, String> headers, @PathVariable String id) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             proveedorService.removeProveedor(id);
+        }else{
+        throw new AuthException("No authorizado");
         }
     }
 
     @PutMapping("/{id}")
-    public void updateProveedor(@RequestHeader Map<String, String> headers, @PathVariable String id, @RequestBody Proveedor proveedor){
+    public void updateProveedor(@RequestHeader Map<String, String> headers, @PathVariable String id, @RequestBody Proveedor proveedor) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             proveedorService.updateProveedor(id, proveedor);
+        }else{
+        throw new AuthException("No authorizado");
         }
     }
 
